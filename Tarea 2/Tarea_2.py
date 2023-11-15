7bd32432b3c4171b82557a60e467b979711feabd
Benjamin Alejandro Pavez Ortiz <benjamin.pavez@usm.cl>
2023-11-14 22:37:37 -0300

    Update Tarea_2.py

--- Tarea 2/Tarea_2.py
+++ Tarea 2/Tarea_2.py
@@ -1,6 +1,7 @@
 import sys
 
 
+
 '''
 La funcion ReadInt recibe el nombre del archivo input, el cual se lee y saca la informacion del numero de cajas y las dimensiones de las cajas.
 
@@ -10,14 +11,11 @@ La funcion ReadInt recibe el nombre del archivo input, el cual se lee y saca la 
     Retorno:
         max_sup : Entero que contiene la superposicion mas grande entre todos los pares de intervalos
 '''
-def max_overlap(A):
-    #Convertir la lista de listas en una lista plana de tuplas
+def MaxSup(A):
     A = [tuple(interval) for interval in A]
-
+    
     #Ordenar la lista de tuplas
     A.sort(key=lambda x: x[0])
-
-    #Inicializar la superposición máxima
     max_sup = 0
 
     def go(alo, ahi):
@@ -35,6 +33,7 @@ def max_overlap(A):
         go(amid + 1, ahi)
 
     go(0, len(A) - 1)
+
     return max_sup
 
 
@@ -57,18 +56,19 @@ def ReadInt():
             tam = int(cadena[0])
         else:
             i = 0
-            while i < (tam * 2) - 1:
+            while i < (tam * 2):
                 sublista = []
                 sublista.append(int(cadena[i]))
                 sublista.append(int(cadena[i + 1]))
                 intervalos.append(sublista)
                 i += 2
-            result = max_overlap(intervalos)
-            print(result+1)
+            result = MaxSup(intervalos)
+            
+            # Cambia la lógica de impresión para reflejar la cantidad de intervalos con superposición
+            print(result + 1 if result > 0 else 0)
             intervalos.clear()
 
 
 
-
-if __name__ == "__main__":
-    ReadInt()
+#Inicio del programa
+ReadInt()
