1ffabe91feb23a79c0fa22f686198492af204658
Benjamin Alejandro Pavez Ortiz <benjamin.pavez@usm.cl>
2023-11-14 21:43:20 -0300

    Update Tarea_2.py

--- Tarea 2/Tarea_2.py
+++ Tarea 2/Tarea_2.py
@@ -1,7 +1,6 @@
-#QUITAR COMENTARIO AL ENVIAR
-############################################################
-       #OBS: Funcion nacho modificada por mi y chagpt
-############################################################
+import sys
+
+
 '''
 La funcion ReadInt recibe el nombre del archivo input, el cual se lee y saca la informacion del numero de cajas y las dimensiones de las cajas.
 
@@ -44,47 +43,32 @@ def max_overlap(A):
 La funcion ReadInt recibe el nombre del archivo input, el cual se lee y saca la informacion del numero de cajas y las dimensiones de las cajas.
 
     Parametros:
-        nombre_arch : String que contiene el nombre del archivo, por defecto sera 'input-1.dat'
+        La funcion no recibe ningun parametro
         
     Retorno:
         La funcion no retorna nada
 '''
-def ReadInt(nombre_arch):
-    print("----------------------------------------------------------------")
-    print("Archivo :",nombre_arch)
+def ReadInt():
     intervalos = []
     tam = 0
-    with open(nombre_arch, 'r') as archivo:
-        tam = 0
-        for linea in archivo:
-            cadena = linea.split()
-            if(len(cadena) == 1):
-                tam = int(cadena[0])
-                print("----------------------------------------------------------------")
-                print("Cantidad de intervalos",tam)
-            else:
-                i = 0
-                while(i<(tam*2)-1):
-                    sublista = []
-                    sublista.append(int(cadena[i]))
-                    sublista.append(int(cadena[i+1]))
-                    intervalos.append(sublista)
-                    i+=2
-                print("Intervalos :",intervalos)
-                result = max_overlap(intervalos)
-                print("Superposicion mas grande :",result)
-                intervalos.clear() #Borro la lista para que no se acumulen los intervalos
+    for linea in sys.stdin:
+        cadena = linea.split()
+        if len(cadena) == 1:
+            tam = int(cadena[0])
+        else:
+            i = 0
+            while i < (tam * 2) - 1:
+                sublista = []
+                sublista.append(int(cadena[i]))
+                sublista.append(int(cadena[i + 1]))
+                intervalos.append(sublista)
+                i += 2
+            result = max_overlap(intervalos)
+            print(result+1)
+            intervalos.clear()
 
 
-def ReadDir():
-    import os
-    contenido = os.listdir('./')
-    for nombre_arch in contenido:
-        if(nombre_arch[-4:] == '.dat'):
-            ReadInt(nombre_arch)
-            print("\n")
-        
 
 
-ReadDir()
-#ReadInt('input-1.dat') #Prueba 1
+if __name__ == "__main__":
+    ReadInt()
